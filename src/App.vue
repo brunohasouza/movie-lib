914894a06ef0681d9ed3c56cf2bcc55c68cbe827
Bruno Souza <souza.brunoha@gmail.com>
2023-11-26 15:47:35 -0300

    feat: add vue-router dependencie

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,16 @@
 <template>
   <v-app>
-
-    <v-main>
-      <v-container fill-height fluid>
-        <v-row no-gutters justify="center" align="center">
-          <v-col md="6">
-            <v-form ref="searchForm" v-model="valid" @submit.prevent="onSubmit">
-              <v-text-field
-                v-model="search"
-                :rules="searchRules"
-                :placeholder="$t('search.placeholder')"
-                prepend-inner-icon="mdi-magnify"
-                rounded
-                filled
-              ></v-text-field>
-            </v-form>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-import { client } from './service/http-client'
+import { client } from '@/service/http-client'
 
 export default {
   name: 'App',
 
-  data: () => ({
-    search: '',
-    valid: true,
-    movies: []
-  }),
-
-  computed: {
-    searchRules() {
-      return [
-        v => {
-          const value = v?.trim()
-
-          return !!value || this.$t('validations.required', { field: 'This field' })
-        }
-      ]
-    }
-  },
+  data: () => ({ }),
 
   methods: {
     fetchMovies() {
@@ -56,11 +22,6 @@ export default {
       }).then(response => console.log(response.data))
         .catch(err => console.error(err))
     },
-
-    onSubmit() {
-      const isValid = this.$refs.searchForm.validate()
-      if (!isValid) return
-    }
   },
 };
 </script>
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomePage from '@/views/HomePage.vue'
+import MoviesPage from '@/views/MoviesPage.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomePage
+  },
+  {
+    path: '/movies',
+    name: 'movies',
+    component: MoviesPage
+  }
+]
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes
+})
