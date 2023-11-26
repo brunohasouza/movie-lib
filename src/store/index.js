71884508fea847d04a8bb5c57398f4668e6e66c8
Bruno Souza <souza.brunoha@gmail.com>
2023-11-26 13:49:08 -0300

    refactor: separate actions, mutations, getters and state into their own files

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import actions from './actions'
+import mutations from './mutations'
+import state from './state'
+import getters from './getters'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state,
+  getters,
+  mutations,
+  actions,
 })
